8192d94d349adb9809ba97fdc189f4df5ccd8ec1
Dzrekey Daniel Kwasi <dkdzrekey001@gmail.com>
2023-08-25 01:06:45 +0000

    A program to print the value of EOF

---
+++ Chapter_1/exercise_1-7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ *
+ * Description: A program to print the value fo EOF.
+ *
+ * Return: Always Nothing
+ */
+
+int main(void)
+{
+	printf("The value of EOF is: %d\n", EOF);
+	return (0);
+}
